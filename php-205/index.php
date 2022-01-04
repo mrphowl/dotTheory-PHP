c6312ffa400499bea6837bb26a154554f7f7ce13
Paul Adraneda <pegadraneda@gmail.com>
2022-01-04 11:18:46 +0800

    PHP 205 practical exam. Initial form

---
+++ php-205/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dotTheory: PHP 205 Practical Exam</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <div>
+            <label for="firstname">First name</l1abel>
+            <input type="text" name="firstname" id="firstname" placeholder="First name">
+        </div>
+        <div>
+            <label for="lastname">Last name</l1abel>
+            <input type="text" name="lastname" id="lastname" placeholder="Last name">
+        </div>
+        <div>
+            <label for="age">Age</l1abel>
+            <input type="number" name="age" id="age" placeholder="Age" min="0">
+        </div>
+        <div>
+            <button type="submit" name="submitted" value="1">Submit</button>
+        </div>
+    </form>
+    
+</body>
+</html>
