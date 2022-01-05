1132820603bcbcd59b18f1c7bd263f24c2db40d3
Paul Adraneda <pegadraneda@gmail.com>
2022-01-05 20:05:40 +0800

    Remove example string in comment

--- php-401/index.php
+++ php-401/index.php
@@ -3,12 +3,6 @@
  * PHP 401 practical exam
  * 
  * Given a string, remove all HTML tags except paragraph and italics tags.
- * 
- * String:
-<p>In Italy<i>pasta</i> is commonly <br />
-eaten during <b>Lunch and Dinner</b> to have a lighter stomach.<br />
-The only thing that is common for Italians is that <i>coffee</i> is always served 3 or 4 times a day.</p>
-<script>alert("Cafe!");</script>
  */
 $samplestring = <<<'EOT'
 <p>In Italy<i>pasta</i> is commonly <br />
